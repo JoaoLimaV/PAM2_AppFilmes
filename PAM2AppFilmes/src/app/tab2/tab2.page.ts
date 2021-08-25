8423b1821b631713879b65d09b3ddbd666b58cfd
João Victor de Lima <joaolima675@outlook.com>
2021-08-25 09:53:29 -0300

    Projeto Atualizado PAM2 c/ Toast e Alert

--- PAM2AppFilmes/src/app/tab2/tab2.page.ts
+++ PAM2AppFilmes/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public  toastController:  ToastController) {}
+
+          async exibirAlertaFavorito() {
+            const alert = await this.alertController.create({
+            header: 'Alerta',
+            message: 'Deseja realmente adicionar este filme aos <strong>favoritos</strong>?',
+            buttons: [
+              {
+                text: 'Cancelar',
+                role: 'cancel',
+                handler: (blah) => {
+                console.log('Confirm Cancel: blah');
+              }
+            }, {
+                text: 'Sim, Adicionar',
+                handler: () => {
+                  this.apresentarToast();
+              }
+            }
+          ]
+        });
+              await alert.present();
+        }
+
+        async apresentarToast(){
+          const toast = await this.toastController.create({
+            message: 'Filme adicionado aos favoritos.',
+            duration: 2000,
+            color: 'success'
+
+          });
+          toast.present();
+        }
 
 }
